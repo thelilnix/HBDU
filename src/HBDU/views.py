50198cfc553060638575943533e9cbd79b291107
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-11-10 10:15:39 +0330

    /v1/say_happy route added

--- src/HBDU/views.py
+++ src/HBDU/views.py
@@ -1,9 +1,27 @@
 from HBDU import app
 from flask import (
     render_template,
+    request,
+    redirect,
+    url_for,
 )
+
+
+THEMES = ["Birthday_1", "Birthday_2", "Birthday_3", "Birthday_4"]
 
 
 @app.route("/")
 def home():
     return render_template("Home/index.html")
+
+
+@app.route("/v1/say_happy", methods=['POST'])
+def say_happy():
+    if (request.form.get("name", '') == ''
+        or request.form.get("author", '') == ''
+        or request.form.get("template", '') == ''
+            or request.form["theme"] not in THEMES):
+        # Required parameters passed and `theme` passed correct
+        return redirect(url_for("home"))
+
+    return redirect(f"/v1/themes/{request.form['theme']}")
